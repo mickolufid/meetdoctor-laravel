0d07a3c8f90276cbbaf9db45aef45eb50bece465
mickolufid <hudzaifah.hilmy@gmail.com>
2022-09-06 17:08:33 +0800

    add database

---
+++ database/migrations/2022_09_06_081252_create_detail_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('type_user_id');
+            $table->string('contact')->nullable();
+            $table->longtext('address')->nullable();
+            $table->longtext('photo')->nullable();
+            $table->enum('gender', [1,2])->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_user');
+    }
+}
