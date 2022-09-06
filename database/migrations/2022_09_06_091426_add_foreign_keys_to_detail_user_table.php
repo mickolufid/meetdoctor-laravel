3652100293538ceef2bf7717142b2b17907bf016
mickolufid <hudzaifah.hilmy@gmail.com>
2022-09-06 18:00:41 +0800

    foreign keys in table detail users

---
+++ database/migrations/2022_09_06_091426_add_foreign_keys_to_detail_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_detail_user_to_users')
+            ->reference('id')->on('users')->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
+
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_users')
+            ->reference('id')->on('type_users')->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->dropForeign('fk_detail_user_to_users');
+            $table->dropForeign('fk_detail_user_to_type_users');
+        });
+    }
+}
